c527c96751bdc013cdbfb828183969839a2d0f50
mayankmishra0801 <mayank08mrinal04@gmail.com>
2023-02-28 04:28:26 +0530

    envoie

--- portfolio_assignment/21_02/simone1.js
+++ portfolio_assignment/21_02/simone1.js
@@ -9,3 +9,22 @@ $(document).ready(function () {
         $('.la-bars').toggleClass('la-times');
     })
 })
+
+
+$(function () {
+    $(".setting").on("click", function () {
+      $(this).parent().toggleClass("show");
+    });
+    $(".side__list__item").on("click", function () {
+      $(this).toggleClass("active");
+      var value = $(this).attr("data-text");
+      $("body").toggleClass(value);
+    });
+    $(".reset").on("click", function () {
+      $(".side__list__item").removeClass("active");
+    });
+    $("input").on("change", function () {
+      $("body").css("--pcolor", $(this).val());
+    });
+  });
+  
